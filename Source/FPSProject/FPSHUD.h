c373bc34fe61ff897cbb39902c85067fab84b86d
Justin Zhang <justin.zhang.1@stonybrook.edu>
2021-10-12 16:20:29 -0400

    Added Hud

--- Source/FPSProject/FPSHUD.h
+++ Source/FPSProject/FPSHUD.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/HUD.h"
+#include "Engine/Canvas.h" 
 #include "FPSHUD.generated.h"
 
 /**
@@ -13,5 +14,15 @@ UCLASS()
 class FPSPROJECT_API AFPSHUD : public AHUD
 {
 	GENERATED_BODY()
+
+protected:
+    // This will be drawn at the center of the screen.
+    UPROPERTY(EditDefaultsOnly)
+        UTexture2D* CrosshairTexture;
+
+public:
+    // Primary draw call for the HUD.
+    virtual void DrawHUD() override;
 	
 };
+
